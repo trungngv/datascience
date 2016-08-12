a0311946e1181527d5387cec39b22a4d23f58c24
Trung Nguyen <trung.nguyen@tabcorp.com.au>
2016-08-12 16:45:40 +1000

    R package install

---
+++ R_package_install_file.R
@@ -0,0 +1,40 @@
+# Useful R packages for data science
+# 
+# Author: Trung Nguyen
+###########################
+install.packages("knitr")
+### data ETL
+install.packages("magrittr")
+install.packages("plyr")
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("reshape2")
+install.packages("data.table")
+install.packages("ggplot2")
+install.packages("stringi")
+install.packages("stringr")
+install.packages("lubridate")
+install.packages("readr")
+### exploratory
+install.packages("DescTools")
+install.packages("gmodels")
+install.packages("Hmisc")
+### models
+install.packages("caret")
+install.packages("glmnet")
+install.packages("gbm")
+install.packages("randomForest")
+install.packages("xgboost")
+# survival, regression, classification
+install.packages("randomForestSRC")
+# analysis with random forest
+install.packages("ggRandomForests")
+# fast nearest neighbors
+install.packages("FNN")
+### evaluation
+install.packages("Metrics")
+install.packages("PerformanceAnalytics")
+install.packages("pROC")
+### parallelization
+install.packages("doParallel")
+install.packages("foreach")
